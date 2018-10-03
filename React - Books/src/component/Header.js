b16d22b4c85b2f6cfe3d26a8413a4afad0f752df
Sazvan <sazvan.kasim.ali@gmail.com>
2018-10-03 21:49:41 +0200

    Add global values, and example on how to use (header color as example)

--- React - Books/src/component/Header.js
+++ React - Books/src/component/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link, NavLink } from "react-router-dom";
+import { Colors } from '../../../values/colors'
+let colors = require('../../../values/colors.json');
 
 class Header extends React.Component {
     render() {
       return (
-        <div>
+        <div style={"background-color: " + colors.header}>
           <ul className="header">
             <li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
             <li><NavLink activeClassName="active" to="/products">Products</NavLink></li>
---
+++ Values/colors.ts
@@ -0,0 +1,8 @@
+export interface Colors{
+    background: string;
+    textColor: string;
+    mainColor: string;
+    accentColor: string;
+    secondColor: string;
+    header: string;
+}
